8a4698794716c3c9a5fb95cc1717230a3a9b0e7b
Renato Wéssmėr <rewesa@outlook.com.br>
2023-05-22 02:08:36 -0300

    Resolução

--- 06 - Lógica de programação/resolucao-problemas/index.js
+++ 06 - Lógica de programação/resolucao-problemas/index.js
@@ -9,3 +9,10 @@ console.log(nomeDoEstudante);
 //Em outras linguagens, portanto, você encontrará algo semelhante a:
 String: nomeEstudante = "Helena";
 
+
+let listaDeEstudantes = ["Helena", "Chico", "Mário"];
+let quantidadeDeEstudantes = listaDeEstudantes.length;
+for (let indice = 0; indice < quantidadeDeEstudantes; indice++) {
+	const alunoCorrente = listaDeEstudantes[indice];
+	console.log(alunoCorrente);
+}
